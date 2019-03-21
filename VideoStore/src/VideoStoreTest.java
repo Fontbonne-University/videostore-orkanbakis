30e1af0274c556eb46a933651199ec3a27eb6382
bakiso <bakiso@R305-10-780.Fontbonne.edu>
2019-03-21 17:47:40 -0500

    Changed the class path to JUNIT4

--- VideoStore/src/VideoStoreTest.java
+++ VideoStore/src/VideoStoreTest.java
@@ -1,43 +1,64 @@
 import junit.framework.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class VideoStoreTest extends TestCase
-{
-	
+import org.junit.Before;
+
+public class VideoStoreTest {
+
 	private Statement statement;
-	
-	public VideoStoreTest (String name) {
-		super (name);
+	final double DELTA = .001;
+
+	@Before
+	public void setUp() {
+		statement = new Statement("Customer");
 	}
-	
-	protected void setUp ()  {
-		statement = new Statement("Fred");
-	}
-	
-	public void testSingleNewReleaseStatement () {
-		statement.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));		
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", statement.generate ());
-		assertEquals(9.0, statement.getTotal());
+
+	@Test
+	public void testSingleNewReleaseStatementTotals() {
+		statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+		statement.generate();
+		assertEquals(9.0, statement.getTotal(), DELTA);
 		assertEquals(2, statement.getFrequenetRenterPoint());
 	}
 
-	public void testDualNewReleaseStatement () {
-		statement.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
-		statement.addRental (new Rental (new Movie ("The Tigger Movie", Movie.NEW_RELEASE), 3));
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", statement.generate ());
+	@Test
+	public void testDualNewReleaseStatementTotals() {
+		statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+		statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
+		statement.generate();
+		assertEquals(18.0, statement.getTotal(), DELTA);
+		assertEquals(4, statement.getFrequenetRenterPoint());
 	}
 
-	public void testSingleChildrensStatement () {
-		statement.addRental (new Rental (new Movie ("The Tigger Movie", Movie.CHILDRENS), 3));
-		assertEquals ("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", statement.generate ());
-	}
-	
-	public void testMultipleRegularStatement () {
-		statement.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
-		statement.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
-		statement.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));
-		
-		assertEquals ("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", statement.generate ());
+	@Test
+	public void testSingleChildrensStatementTotals() {
+		statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
+		statement.generate();
+		assertEquals(1.5, statement.getTotal(), DELTA);
+		assertEquals(1, statement.getFrequenetRenterPoint());
 	}
 
-	
+	@Test
+	public void testMultipleRegularStatementTotals() {
+		statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
+		statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
+		statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+		statement.generate();
+		assertEquals(7.5, statement.getTotal(), DELTA);
+		assertEquals(3, statement.getFrequenetRenterPoint());
+	}
+
+	@Test
+	public void testMultipleRegularStatementFormat() {
+		statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
+		statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
+		statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+
+		assertEquals(
+				"Rental Record for Customer\n" + "\tPlan 9 from Outer Space\t2.0\n"
+						+ "\t8 1/2\t2.0\n\tEraserhead\t3.5\n" + "You owed 7.5\nYou earned 3 frequent renter points\n",
+				statement.generate());
+	}
+
 }
